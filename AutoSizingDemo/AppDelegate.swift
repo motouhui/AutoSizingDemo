adf1771ad1a566ac42ef2f02f9d6e1f6cccab230
Arthur Wang <arthraim@gmail.com>
2016-01-07 17:06:27 +0800

    done

--- AutoSizingDemo/AppDelegate.swift
+++ AutoSizingDemo/AppDelegate.swift
@@ -12,10 +12,16 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    var viewController: ViewController?
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        viewController = ViewController()
+        if let window = window, viewController = viewController {
+            window.rootViewController = viewController
+            window.makeKeyAndVisible()
+        }
         return true
     }
 
--- AutoSizingDemo/ViewController.swift
+++ AutoSizingDemo/ViewController.swift
@@ -7,17 +7,44 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
+    var scrollView: UIScrollView!
+    var label: UILabel!
+    var textView: UITextView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        view.backgroundColor = UIColor.whiteColor()
+
+        scrollView = UIScrollView()
+        view.addSubview(scrollView)
+        scrollView.snp_makeConstraints { (make) -> Void in
+            make.top.equalTo(view).offset(20)
+            make.right.bottom.left.equalTo(view)
+        }
+
+        label = UILabel()
+        label.text = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt"
+        label.numberOfLines = 0
+        label.font = UIFont.boldSystemFontOfSize(30)
+        scrollView.addSubview(label)
+        label.snp_makeConstraints { (make) -> Void in
+            make.top.width.centerX.equalTo(scrollView)
+        }
+
+        textView = UITextView()
+        textView.text = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        textView.font = UIFont.systemFontOfSize(20)
+        textView.scrollEnabled = false
+        scrollView.addSubview(textView)
+        textView.snp_makeConstraints { (make) -> Void in
+            make.top.equalTo(label.snp_bottom)
+            make.width.centerX.bottom.equalTo(scrollView)
+        }
     }
 
 
